38ad3e6449a53e9bbcbfaa01ff1070734f6222fc
TylerSakura <466796756@qq.com>
2023-05-17 14:36:51 +1000

    by tianke

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 import dat from "https://unpkg.com/dat.gui@0.7.7/build/dat.gui.module.js"
 
 import { STLLoader } from "https://cdn.skypack.dev/three@0.133.1/examples/jsm/loaders/STLLoader.js";
@@ -81,7 +80,7 @@ scene.add(light);
 const world = new CANNON.World();
 world.broadphase = new CANNON.NaiveBroadphase();
 world.gravity.set(0, -10, 0);
-world.defaultContactMaterial.friction = 0;
+world.defaultContactMaterial.friction = 0.002;
 world.broadphase = new CANNON.SAPBroadphase(world);
 world.solver.iterations = 10;
 const cannonDefaultMaterial = new CANNON.Material()
@@ -90,8 +89,8 @@ const cannonDefaultCantactMaterial = new CANNON.ContactMaterial(
   cannonDefaultMaterial,
   cannonDefaultMaterial,
   {
-    friction: 0.31, // 摩擦力
-    restitution: 0.3, // 弹性系数
+    friction: 0.002, // 摩擦力
+    restitution: 0.002, // 弹性系数
     contactEquationStiffness: 1000
   }
 );
@@ -143,29 +142,19 @@ scene.add(meshfin)
 var textureLoader = new THREE.TextureLoader();
 var glassTexture = textureLoader.load('../assets/images/glass.jpg');
 // 创建地面three
-var vertices = [];
-var faces = [];
-const threePlaneGeometry = new THREE.PlaneGeometry(1000,1000 ,100, 100);
- for (var i = 0; i < threePlaneGeometry.vertices.length; i++) {
-  var vertex = threePlaneGeometry.vertices[i];
-  vertices.push(new CANNON.Vec3(vertex.x, vertex.y, vertex.z));
-  vertex.z = Math.random() * 2;
-}
-
-threePlaneGeometry.computeVertexNormals();
+const threePlaneGeometry = new THREE.PlaneGeometry(1000,1000);
 const planeMaterial = new THREE.MeshBasicMaterial({ color: 0x999999 });
-
 glassTexture.wrapS = THREE.RepeatWrapping;
 glassTexture.wrapT = THREE.RepeatWrapping;
 glassTexture.repeat.set(100, 100); 
 planeMaterial.map = glassTexture;
- let threePlaneMesh = new THREE.Mesh( threePlaneGeometry,planeMaterial);
- threePlaneMesh.rotation.x = -Math.PI/2;
-  threePlaneMesh.castShadow = true;
- threePlaneMesh.receiveShadow = true;
- threePlaneMesh.position.set(0,0,0);
-
+let threePlaneMesh = new THREE.Mesh( threePlaneGeometry,planeMaterial);
+threePlaneMesh.rotation.x = -Math.PI/2;
+threePlaneMesh.castShadow = true;
+threePlaneMesh.receiveShadow = true;
+threePlaneMesh.position.set(0,0,0);
 scene.add( threePlaneMesh );
+
  //地面incannon
 let cannonPlanShape = new CANNON.Plane();
 let cannonPlanMaterial = new CANNON.Material();
@@ -234,6 +223,7 @@ loaderFBX.load('../assets/car.fbx', function (fbx) {
    });
   body.addShape(shape);
   body.position.set(0, 1, 1);
+  body.quaternion.setFromAxisAngle(new CANNON.Vec3(0, 1, 0), Math.PI); // 设置初始旋转角度为180度
   world.addBody(body);
   MeshBodyToUpdate.push({mesh:mesh,body:body});
   // 在车辆模型加载完成之后，再加载车轮模型
@@ -274,27 +264,62 @@ loaderFBX.load('../assets/car.fbx', function (fbx) {
     wheels[2].position.set(0.8, 0.2, -1.1); // Modify the position of the third wheel
     wheels[3].position.set(-0.8, 0.2, -1.1); // Modify the position of the fourth wheel    
   });
-  document.addEventListener("keydown", onDocumentKeyDown, false);
-  function onDocumentKeyDown(event) {
-    var keyCode = event.which;
-    if (keyCode == 87) {
-		body.position.z += zSpeed;
-		
-    } else if (keyCode == 83) {
-      body.position.z -= zSpeed;
-		
-    } else if (keyCode == 65) {
-      body.position.x -= xSpeed;
-		
-    } else if (keyCode == 68) {
-      body.position.x += xSpeed;
-		
-    } 
-};
+// 全局变量
+const forceMultiplier = 50;
+
+// 声明一个对象来记录哪些键正在被按下
+const keys = {};
+
+// 当按下键盘按键时，将对应的状态设为 true
+document.addEventListener('keydown', (event) => {
+  keys[event.key] = true;
 });
 
+// 键盘事件处理器
+document.addEventListener('keydown', (event) => {
+  const key = event.key;
+  const force = new CANNON.Vec3();
 
+  if (key === 'w' || key === 'W') { // W 键
+    const forwardVector = new CANNON.Vec3(0, 0, 0.3); // 定义一个向前的向量
+    force.copy(body.quaternion.vmult(forwardVector)); // 将向前的向量根据车辆的朝向进行旋转
+    force.scale(forceMultiplier, force); // 根据力的大小进行缩放
+    body.velocity.copy(force); // 更新速度向量
+  } else if (key === 's' || key === 'S') { // S 键
+    const backwardVector = new CANNON.Vec3(0, 0, -0.3); // 定义一个向后的向量
+    force.copy(body.quaternion.vmult(backwardVector)); // 将向后的向量根据车辆的朝向进行旋转
+    force.scale(forceMultiplier, force); // 根据力的大小进行缩放
+    body.velocity.copy(force); // 更新速度向量
+  } 
+  
+  if (key === 'a' || key === 'A') { // A 键
+    body.angularVelocity.set(0, 2, 0); // 设置角速度，使车辆向左旋转
+    body.velocity.copy(body.quaternion.vmult(new CANNON.Vec3(0, 0, -xSpeed))); // 更新速度向量
+  } else if (key === 'd' || key === 'D') { // D 键
+    body.angularVelocity.set(0, -2, 0); // 设置角速度，使车辆向右旋转
+    body.velocity.copy(body.quaternion.vmult(new CANNON.Vec3(0, 0, -xSpeed))); // 更新速度向量
+  }
 
+  // 施加力到 body 对象
+  if (!force.isZero()) {
+    body.applyForce(force, body.position);
+  }
+});
+
+// 当释放键盘按键时，将对应的状态设为 false
+document.addEventListener('keyup', (event) => {
+  const key = event.key;
+
+  if (key === 'w' || key === 'W' || key === 's' || key === 'S') { // 释放W或S键
+    body.velocity.set(0, 0, 0); // 停止移动
+  } else if (key === 'a' || key === 'A' || key === 'd' || key === 'D') { // 释放A或D键
+    body.angularVelocity.set(0, 0, 0); // 停止旋转
+  }
+});
+
+  
+
+});
 
 // animate
 
@@ -307,21 +332,43 @@ meshfin.rotation.y += 0.01;
 	for(const object of MeshBodyToUpdate){
 		object.mesh.position.copy(object.body.position);
 		object.mesh.quaternion.copy(object.body.quaternion);
-     camera.position.copy(object.mesh.position);
-      camera.position.y += 10;
-      camera.position.z += 15;
+    camera.position.copy(object.mesh.position);
+    camera.position.y += 10;
+    camera.position.z += 15;
     camera.lookAt(object.mesh.position);
 
+
+    // 在这里添加约束
+    //if (object.body === body) {
+    //  const worldUp = new CANNON.Vec3(0, 1, 0);
+    //  const bodyUp = new CANNON.Vec3();
+    //  object.body.quaternion.vmult(worldUp, bodyUp);
+    //  const quat = new CANNON.Quaternion();
+    //  quat.setFromVectors(bodyUp, worldUp);
+    //  object.body.quaternion.vmult(quat, object.body.quaternion);
+    //}
 	}
   // 如果车辆正在移动，旋转车轮
-  if (body.velocity.length() > 0) {
+  if (body && body.velocity.length() > 0) {
     // 旋转车轮，这里假设沿着x轴旋转，你可以根据需要修改
     for (const wheel of wheels) {
         wheel.rotation.x += 0.01;
     }
 }
     renderer.render( scene, camera );
-    
+
+// 根据按键状态更新速度向量
+const forwardVector = new CANNON.Vec3(0, 0, -1); // 向前的向量
+const backwardVector = new CANNON.Vec3(0, 0, 1); // 向后的向量
+const force = new CANNON.Vec3();
+
+if (isMovingForward) { // 按下 "W" 键，向前移动
+  force.copy(body.quaternion.vmult(forwardVector).scale(xSpeed));
+  body.applyForce(force, body.position);
+} else if (isMovingBackward) { // 按下 "S" 键，向后移动
+  force.copy(body.quaternion.vmult(backwardVector).scale(xSpeed));
+  body.applyForce(force, body.position);
+}
 }
 animate();
 }
